2c086b728132ad8e827d93eca8a9bb912eef64f3
matthcol <matthias.colin@gmail.com>
2023-02-21 11:29:20 +0100

    before splitting code

--- Basics/app.c
+++ Basics/app.c
@@ -77,9 +77,38 @@ void playWithStrings(){
     free(newday);
 }
 
-int main(){
+void swap(double *p_x,double *p_y) {
+    double tmp;
+    tmp = *p_x;
+    *p_x = *p_y;
+    *p_y = tmp;
+}
+
+void playWithSwap() {
+    double a = 3.14;
+    double b = 365.25;
+    printf("Before swap: %f, %f\n", a, b);
+    swap(&a,&b);
+    printf("Before swap: %f, %f\n", a, b);
+}
+
+// int main(){
+// int main(int argc, char *argv[]){ 
+int main(int argc, char *argv[], char *arge[]){     
+    printf("App started with %d args:\n", argc);
+    for (int i=0; i < argc; i++) {
+        printf("\t[%d] = %s\n", i, argv[i]);
+    }
+    printf("Environment:\n");
+    for (; *arge != NULL; arge++) {
+    // for (int i=0; i < 6; i++) {
+        printf("\t%s\n", *arge);
+    }
     // playWithInts();
     // playWithDoubles();
     // bool ok = isWeatherOk();
-    playWithStrings();
+    // playWithStrings();
+    // playWithSwap();
+    
+    return 0;
 }
